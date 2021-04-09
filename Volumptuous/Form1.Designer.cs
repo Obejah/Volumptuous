dbed2e6a46809a42ab109faa3f6c82f18259976a
Obejah <obejah3@outlook.com>
2021-04-09 17:44:43 +0200

    Add project files.

---
+++ Volumptuous/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Volumptuous
+{
+    partial class FlappyBird
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlappyBird));
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.idle = new System.Windows.Forms.Timer(this.components);
+            this.Start = new System.Windows.Forms.Button();
+            this.Leaderboards = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Ground = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
+            this.Player.Location = new System.Drawing.Point(68, 325);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(91, 64);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(21, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lives: 3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(600, 184);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 345);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Pipe";
+            // 
+            // idle
+            // 
+            this.idle.Enabled = true;
+            this.idle.Interval = 50;
+            this.idle.Tick += new System.EventHandler(this.idle_Tick);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(192, 206);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(121, 51);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Leaderboards
+            // 
+            this.Leaderboards.Location = new System.Drawing.Point(446, 206);
+            this.Leaderboards.Name = "Leaderboards";
+            this.Leaderboards.Size = new System.Drawing.Size(121, 51);
+            this.Leaderboards.TabIndex = 4;
+            this.Leaderboards.Text = "Leaderboards";
+            this.Leaderboards.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(319, 253);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(121, 51);
+            this.Exit.TabIndex = 5;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Ground
+            // 
+            this.Ground.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Ground.BackgroundImage")));
+            this.Ground.Location = new System.Drawing.Point(-1, 895);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(1189, 108);
+            this.Ground.TabIndex = 6;
+            this.Ground.TabStop = false;
+            this.Ground.Tag = "Pipe";
+            // 
+            // FlappyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(752, 985);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Leaderboards);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FlappyBird";
+            this.Text = "FlappyBird";
+            this.Load += new System.EventHandler(this.Volomptuous_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer idle;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Leaderboards;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.PictureBox Ground;
+    }
+}
+
---
+++ Volumptuous/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volumptuous
+{
+    public partial class FlappyBird : Form
+    {
+        bool gameover = false;
+
+        int gamespeed = 5;
+        int pipespeed = 10;
+        int gravity = 8;
+        int scorer1 = 0;
+        int scorer2 = 0;
+        int scorer3 = 0;
+        int score = 0;
+        int lives = 3;
+        int totalscore = 0;
+        int idlespeed = 5;
+        public FlappyBird()
+        {
+            InitializeComponent();
+        }
+
+        private void endgame()
+        {
+            if (gameover = true)
+            {
+                timer1.Stop();
+             
+
+            }
+            timer1.Enabled = false;
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity -= gravity;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "Lives: " + lives;
+
+            Player.Top += gravity;
+            //Pipe1.Left -= pipespeed;
+            //Pipe2.Left -= pipespeed;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "Pipe")
+
+                    if (Player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        lives = lives - 1;
+                        timer1.Stop();
+                        label2.Text = "press space to reset";
+                    }
+            }
+
+            if (score == gamespeed)
+            {
+                pipespeed++;
+                gamespeed = gamespeed + 2;
+            }
+
+            if (lives == 0)
+            {
+                gameover = true;
+            }
+        }
+
+        private void idle_Tick(object sender, EventArgs e)
+        {
+            Player.Top -= idlespeed;
+            if (Player.Top < 300)
+            {
+                idlespeed = -idlespeed;
+            }
+            Player.Top -= idlespeed;
+            if (Player.Top < 600)
+            {
+                idlespeed = -idlespeed;
+            }
+            Ground.Left -= pipespeed;
+
+            if(Ground.Left < -185)
+            {
+                Ground.Left = 0;
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            idle.Stop();
+            timer1.Start();
+            Start.Hide();
+            Leaderboards.Hide();
+            Exit.Hide();
+        }
+
+        private void Volomptuous_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
